536b92ea7eb4e63ea98622d7d1ce8aadf6a2f4f4
Dmytro Moroz <dmytro.moroz3@gmail.com>
2026-07-28 19:52:34 +0200

    Task resolved 88-merge-sorted-array

---
+++ 88-merge-sorted-array/solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1  # pointer to the last relevant element in nums1
+        j = n - 1  # pointer to the last element in nums2
+        # pointer to the last position in nums1 (where we will store the sorted number)
+        k = m + n - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
